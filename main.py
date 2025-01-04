51e9a2d404e4ef6c8449db7886ea23130c08691a
lekanjava <lekanjava@gmail.com>
2025-01-04 07:19:44 +0000

    updating code for azure web app

--- main.py
+++ main.py
@@ -52,5 +52,9 @@ def index():
         return render_template("index.html", strength=strength, password=password)
     return render_template("index.html", strength=None)
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8000)
+
